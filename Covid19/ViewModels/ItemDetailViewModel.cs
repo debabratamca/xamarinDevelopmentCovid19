94388b3d210bba26f256cb9ff49852fbe35ab9ce
debabratamca <debu_mca07@hotmail.com>
2020-04-16 19:56:01 +0530

    added

---
+++ Covid19/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Covid19.Models;
+
+namespace Covid19.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
